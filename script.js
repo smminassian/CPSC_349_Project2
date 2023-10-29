e49dc6a8c44e55dbef3374130cdd3518322721b8
smminassian <sebastienminassian203@gmail.com>
2023-10-29 16:27:12 -0700

    fixed the issues by moving script to the bottom of body

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function FillEmptyCells(){
-    const cells = document.querySelectorAll("cellText");
+    const cells = document.querySelectorAll(".cellText");
     for (const cell of cells ){
         if(cell.innerHTML == ""){
-            (cell.innerHTML =`<span>${((Math.ceil(Math.random() * 2) + 1))}</span>`) || (cell.innerHTML = `<span>${(Math.ceil(Math.random() * 4)+ 3)}</span>`);
+            (cell.innerHTML =`<span>${((Math.ceil(Math.random() * 2) + 1))}</span>`) || (cell.innerHTML = `<span>${((Math.ceil(Math.random() * 4) + 3))}</span>`);
         }
-        else if((cell.innerHTML == 1) || (cell.innerHTML == 3)){
-            const AddDecimal = cell.innerHTML + 0.1;
-            const roundUP = ceil(AddDecimal);
+        else if((cell.innerHTML === 1) || (cell.innerHTML === 3)){
+            const AddDecimal = parseFloat(cell.innerHTML) + 0.1;
+            const roundUP = Math.ceil(AddDecimal);
             cell.innerHTML = `<span>${roundUP}</span>`
         }
         
-        
-
     }
     
 
 }
+
+FillEmptyCells();
