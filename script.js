2bb00875818ca53a9f2d48c18525ec1fa804d99a
smminassian <sebastienminassian203@gmail.com>
2023-10-29 17:36:45 -0700

    3 is not rounding to 4

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function FillEmptyCells(){
     const cells = document.querySelectorAll(".cellText");
     for (const cell of cells ){
-        if(cell.innerHTML == ""){
-            (cell.innerHTML =`<span>${((Math.ceil(Math.random() * 2) + 1))}</span>`) || (cell.innerHTML = `<span>${((Math.ceil(Math.random() * 4) + 3))}</span>`);
+        if(cell.innerHTML === ""){
+            cell.innerHTML = `<span>${((Math.ceil(Math.random() * 2) + 1))}</span>`;
         }
-        else if((cell.innerHTML === 1) || (cell.innerHTML === 3)){
-            const AddDecimal = parseFloat(cell.innerHTML) + 0.1;
-            const roundUP = Math.ceil(AddDecimal);
-            cell.innerHTML = `<span>${roundUP}</span>`
+        else if (cell.innerHTML == 3) { 
+            const addDecimal = (cell.innerHTML) + 0.1;
+            const roundUp = Math.ceil(addDecimal);
+            cell.innerHTML = `<span>${roundUp}</span>`;
         }
-        
     }
     
-
-}
+        
+    }
 
 FillEmptyCells();
