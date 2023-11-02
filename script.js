25be77c61ea39e26722cf7d817fcf4fc88bfc1bd
eynomr <a99omrani@gmail.com>
2023-11-02 00:31:08 -0700

    similify logic

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ function addRandomNumber() {
 
     if (randomCell) {
         //auto generates 4 on moves that are multiples of 10
-        if (count < 2 || count == 10 || count == 20 || count == 30 || count == 40 || count == 50 || count == 60){
+        if (count % 10 === 0 && count !== 0){
             randomCell.textContent = '4';
             gameBoard[y][x] = 4;
         }
